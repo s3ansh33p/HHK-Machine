7dc1077ff7528498817260de9740453e8ee973e2
Sean McGinty <newfolderlocation@gmail.com>
2025-02-24 02:31:42 +0800

    Initial WIP

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+require 'socket'
+
+def local_ip
+  Socket.ip_address_list.detect(&:ipv4_private?).ip_address
+end
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "gusztavvargadr/windows-server-2022-standard"
+  
+  config.vm.provider "hyperv" do |h|
+    h.memory = 4096  
+    h.cpus = 2 
+  end
+  
+  config.vm.network "forwarded_port", guest: 3389, host: 3390, auto_correct: true
+
+  config.vm.synced_folder ".", "/vagrant", type: "smb", smb_host: local_ip
+  
+  # Stage install files
+  config.vm.provision "file", source: "git_install.inf", destination: "C:\\tmp\\git_install.inf"
+  
+  # For testing, run always
+  config.vm.provision "shell", path: "startup.ps1", run: "always"
+
+end
---
+++ startup.ps1
@@ -0,0 +1,81 @@
+Write-Output "Windows Server is launching..."
+Set-Content -Path "C:\tmp\log.txt" -Value "Boot script executed at $(Get-Date)"
+
+# [>] Setup Repositories
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope CurrentUser
+Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
+
+function Add-PathEntry {
+  param (
+    $To
+  )
+  $env:Path += ";$To"
+  [Environment]::SetEnvironmentVariable(
+    "Path",
+    [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User) + ";$To",
+    [EnvironmentVariableTarget]::User)
+}
+
+function Install-MinGW {
+  Write-Output "Installing MinGW..."
+  $ProgressPreference = 'SilentlyContinue'
+
+  $Download64 = "https://github.com/niXman/mingw-builds-binaries/releases/download/14.2.0-rt_v12-rev1/x86_64-14.2.0-release-posix-seh-ucrt-rt_v12-rev1.7z"
+  $Download32 = "https://github.com/niXman/mingw-builds-binaries/releases/download/14.2.0-rt_v12-rev1/i686-14.2.0-release-posix-dwarf-ucrt-rt_v12-rev1.7z"
+
+  Write-Output "Downloading mingw64..."
+  Invoke-WebRequest "$Download64" -OutFile ".\amd64.7z"
+  Write-Output "Extracting mingw64..."
+  Expand-7Zip -ArchiveFileName "amd64.7z" -TargetPath ".\"
+  Remove-Item -Path ".\amd64.7z" -Force
+
+  Write-Output "Downloading mingw32..."
+  Invoke-WebRequest "$Download32" -OutFile ".\i686.7z"
+  Write-Output "Extracting mingw32..."
+  Expand-7Zip -ArchiveFileName "i686.7z" -TargetPath ".\"
+  Remove-Item -Path ".\i686.7z" -Force
+
+  $ProgressPreference = 'Continue'
+
+  $dest = "$env:APPDATA\mingw-dual"
+  Write-Output "Writing to $dest"
+  md -Force $dest | Out-Null
+  Move-Item -Path ".\*" -Destination "$dest"
+
+  Write-Output "Adding mingw64 to PATH..."
+  Add-PathEntry -To "$dest\mingw64\bin"
+  Write-Output "Adding mingw32 to PATH..."
+  Add-PathEntry -To "$dest\mingw32\bin"
+  Write-Output "Done!"
+}
+
+# [>] Install Git
+Write-Output "Installing Git..."
+Install-Module -Name Git -Force -Scope CurrentUser
+Write-Output "Git installed successfully"
+
+# [>] Install 7-Zip
+Write-Output "Installing 7-Zip..."
+Install-Module -Name 7Zip4PowerShell -Force -Scope CurrentUser
+Write-Output "7-Zip installed successfully"
+
+# [>] Install MinGW
+$origin = (Get-Item .).FullName
+$work = Join-Path $Env:Temp $(New-Guid)
+New-Item -Type Directory -Path $work | Out-Null
+try {
+    cd "$work"
+    Install-MinGW
+} finally {
+    cd "$origin"
+    Remove-Item -LiteralPath "$work" -Force -Recurse
+}
+
+# [>] Clone Hollyhock-3
+Write-Output "Cloning Hollyhock-3..."
+git clone "https://github.com/ClasspadDev/hollyhock-3.git" "C:\hollyhock-3"
+Write-Output "Hollyhock-3 cloned successfully"
+
+# [>] Cleanup
+Add-Content -Path "C:\tmp\log.txt" -Value "Boot script completed at $(Get-Date)"
